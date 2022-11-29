72da267d5c82d0dc791cff18e141e38edcee35ef
iman254 <stephaniemarquinz@icloud.com>
2022-11-30 01:52:02 +0200

    8-uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# Author - Bamidele Adefolaju
+
+def uppercase(str):
+    """Print a string in uppercase."""
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
