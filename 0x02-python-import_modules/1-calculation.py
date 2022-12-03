afbf652134460cea99b293d649a8aea2eed80a85
iman254 <stephaniemarquinz@icloud.com>
2022-12-03 22:05:09 +0200

    correction

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,7 +2,7 @@
 
 if __name__ == "__main__":
     """Print the sum, subtraction, multiplication and division of 10 and 5."""
-    from calculator_1 import add, sub, mul, div
+    from calculation_1 import add, sub, mul, div
 
     a = 10
     b = 5
