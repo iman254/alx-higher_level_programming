f53df0b2239747ab6e1ac08276f294d2db35fb81
iman254 <stephaniemarquinz@icloud.com>
2022-12-04 23:23:45 +0200

    4-hidden_discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,8 +3,8 @@
 if __name__ == "__main__":
     """print all the names defined by the compiled module"""
     import hidden_4
-   
+ 
     names = dir(hidden_4)
     for name in names:
         if name[:2] != "__":
-            print(name) 
+            print(name)
