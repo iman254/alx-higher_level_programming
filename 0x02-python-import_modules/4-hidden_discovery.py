4d26a0afce65cc8068998bbd75b972645fcdbd1a
iman254 <stephaniemarquinz@icloud.com>
2022-12-04 23:22:20 +0200

    4-hidden_discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-if __name__== "__main__":
+if __name__ == "__main__":
     """print all the names defined by the compiled module"""
-    import hidden_4  
+    import hidden_4
    
     names = dir(hidden_4)
     for name in names:
