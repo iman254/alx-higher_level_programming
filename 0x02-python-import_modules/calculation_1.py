cb70e5e6eb79c402df640c98662baa851a003a5f
iman254 <stephaniemarquinz@icloud.com>
2022-12-04 21:45:02 +0200

    corrections

--- 0x02-python-import_modules/calculation_1.py
+++ 0x02-python-import_modules/calculation_1.py
@@ -1,54 +1,52 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 
 def add(a, b):
     """My addition function
 
-    args:
+    Args:
         a: first integer
         b: second integer
 
     Returns:
-        the return value. a + b
+        The return value. a + b
     """
-
     return (a + b)
+
 
 def sub(a, b):
     """My subtraction function
 
-    args:
+    Args:
         a: first integer
         b: second integer
 
     Returns:
-        the return value. a - b
+        The return value. a - b
     """
-
     return (a - b)
 
-def mul(a, b):
 
+def mul(a, b):
     """My multiplication function
-    args:
+
+    Args:
         a: first integer
         b: second integer
-   
+
     Returns:
-        the return value. a * b
+        The return value. a * b
     """
-    
     return (a * b)
 
-def div(a, b):
 
+def div(a, b):
     """My division function
-    args:
+
+    Args:
         a: first integer
         b: second integer
+
     Returns:
-        the return value. a / b
+        The return value. a / b
     """
-    
     return int(a / b)
-
-
