e3df463e1eff2dd1f7b83114c3a8301d002a23d9
iman254 <stephaniemarquinz@icloud.com>
2022-12-06 02:20:01 +0200

    8-multiple_returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -5,5 +5,5 @@ def multiple_returns(sentence):
     if len(sentence) == 0:
         my tuple = 0, "None"
     else:
-        my_tuple = len(sentence), semtemce[0]
+        my_tuple = len(sentence), sentence[0]
     return my_tuple
