fcc6917f4cab0fb74f8d1b7d78a7b151817d2f33
iman254 <stephaniemarquinz@icloud.com>
2022-12-20 10:51:33 +0200

    task0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,8 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i], end = ""))
             counter += 1
         except IndexError: 
-            pass 
+            break 
+             
     print("")
     return (counter) 
 
