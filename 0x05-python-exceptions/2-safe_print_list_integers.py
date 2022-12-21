04bc2e170db094d1c5535566bc46201b8216efb1
iman254 <stephaniemarquinz@icloud.com>
2022-12-21 13:34:26 +0200

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers.py(my_list=[], x=0):
+    counter = 0
+    try:
+        for i in range(x):
+            print("{:d}".format(my_list[i]), end="")
+            counter += 1
+    except(TypeError, ValueError):
+            pass
+    print("")
+    return (counter)
