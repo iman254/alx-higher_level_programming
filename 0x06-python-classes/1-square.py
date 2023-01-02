a2bca7f2f9b590cf8ef56e3cc96e0c9e9abcb6e7
iman254 <stephaniemarquinz@icloud.com>
2023-01-02 08:06:01 +0200

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 """defining a cass Square."""
+
+
 class Square:
     """To represent a square in a class."""
     def __init__(self, size):
         """initialize the attribute size and make it private"""
-        self.__size = size 
- 
-    
+        self.__size = size
