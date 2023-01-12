724eebe5e77140e74f91b3266685f3523acc0e77
iman254 <stephaniemarquinz@icloud.com>
2023-01-12 14:15:26 +0200

    task 2

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
 matrix = [
@@ -7,3 +8,7 @@ matrix = [
 ]
 print(matrix_divided(matrix, 3))
 print(matrix)
+
+guillaume@ubuntu:~/0x07$ ./2-main.py
+[[0.33, 0.67, 1.0], [1.33, 1.67, 2.0]]
+[[1, 2, 3], [4, 5, 6]]
