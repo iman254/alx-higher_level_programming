cf1206b9b77c227a18507ed6d5a98889141b99b6
iman254 <stephaniemarquinz@icloud.com>
2023-02-16 20:37:50 +0200

    task corrections

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- creates a table called first_table in database
-CREATE TABLE if NOT EXISTS 'first_table' (
-'id' INT, 'name' VARCHAR(256));
+CREATE TABLE if NOT EXISTS first_table (
+id INT, name VARCHAR(256));
