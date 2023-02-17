5b15ee28935525a14aab40db6a867f574bb2e507
iman254 <stephaniemarquinz@icloud.com>
2023-02-17 22:12:06 +0200

    task 2

---
+++ 0x0E-SQL_more_queries/2-2-create_read_user.sql
@@ -0,0 +1,4 @@
+--script that creates a database and a user
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXIST 'user_0d_2'@'localhost' IDENTIFIED BY 'User_0d_2_pwd';
+GRANT SELECTED ON hbtn_0d_2.* TO 'user_0d_2'@'localhost'
