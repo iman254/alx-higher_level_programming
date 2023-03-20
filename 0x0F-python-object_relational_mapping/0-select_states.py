5e36560bf186d252fbf029a25e0898c3a92fc08f
iman254 <stephaniemarquinz@icloud.com>
2023-03-20 15:11:15 -0700

    corrected a line

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@
 import sys
 import MySQLdb
 
-if __name == "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     c = db.cursor()
     c.execute("""SELECT * FROM states ORDER BY states.id ASC""")
