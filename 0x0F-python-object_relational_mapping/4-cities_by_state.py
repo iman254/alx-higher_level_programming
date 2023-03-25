943ce669101fa89b1715f0891f839de02729b086
iman254 <stephaniemarquinz@icloud.com>
2023-03-25 03:51:19 +0200

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """script that lists all the cities in database hbtn_0e_4_usa"""
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     import sys
     import MySQLdb
 
--- 0x0F-python-object_relational_mapping/4-cities_by_state.sql
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.sql
@@ -1,17 +1,17 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0e_4_usa;
 USE hbtn_0e_4_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id INT NOT NULL AUTO-INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );
 INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("Nevada");
 
 CREATE TABLE IF NOT EXISTS cities (
-    id INT NOT NULL AUTO-INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
     FOREIGN KEY(state_id) REFERENCES states(id)
 );
 
