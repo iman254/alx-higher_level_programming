a5b32642e763513f94aec72b409262ca8befb1d0
iman254 <stephaniemarquinz@icloud.com>
2023-03-25 23:31:58 +0200

    task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,35 @@
 #!/usr/bin/python3
-"""script that takes an adrguement and lists the information regarding the selected argument"""
+
+"""
+    A script that lists all cities in a state from the database hbtn_0e_0_usa
+    Username, password and database name and state are given as user args
+"""
+
+
+import sys
+import MySQLdb
+
 
 if __name__ == '__main__':
-    import sys
-    import MySQLdb
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
 
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], host='localhost', port=3306)
     cursor = db.cursor()
-    
-    query = """ SELECT cities.name FROM states INNER JOIN cities ON states.id = cities.state_id ORDER BY cities.id ASC"""
 
-    cursor.execute(query)
+    sql = """SELECT cities.name
+          FROM states
+          INNER JOIN cities ON states.id = cities.state_id
+          WHERE states.name = %s
+          ORDER BY cities.id ASC"""
+
+    cursor.execute(sql, (sys.argv[4],))
 
     data = cursor.fetchall()
 
-    print(", ".join(city[0] from city in data)
+    print(", ".join([city[0] for city in data]))
+
     cursor.close()
     db.close()
