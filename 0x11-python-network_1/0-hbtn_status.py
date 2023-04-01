47e08a2af38d807286537c8ce35ccff32dd965ff
iman254 <stephaniemarquinz@icloud.com>
2023-04-01 04:17:28 +0300

    task 0 corrections

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,8 +3,7 @@
 if __name__ == "__main__":
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')
-    as file:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as file:
         content = file.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
