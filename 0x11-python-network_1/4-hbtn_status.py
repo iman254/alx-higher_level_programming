59c1cb532fb33951ec8fa72fec9199be6ada4a11
iman254 <stephaniemarquinz@icloud.com>
2023-04-01 05:47:14 +0300

    tasks

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""fetches a url with the package request"""
+"""get request with requests package"""
+
+import requests
 
 if __name__ == "__main__":
-    import urllib.requests
-
-    response = urllib.requests.get("https://alx-intranet.hbtn.io/status").text
+    response = requests.get("https://alx-intranet.hbtn.io/status").text
     print("Body response:")
-    print(f"\t- type: {type(response)}")
-    print(f"\t- content: {response}")
+    print(f'\t- type: {type(response)}')
+    print(f'\t- content: {response}')
