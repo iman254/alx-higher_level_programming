afce6ee2e40a3355fe4b995989d0b2bcb0e8cff0
iman254 <stephaniemarquinz@icloud.com>
2023-04-01 05:02:17 +0300

    task 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script that takes in an email and a url and sends a post request to the passed url with the email as parameter and displays body in decoded utf-8
+"""
+
+if __name__ == "__main__":
+    import sys
+    from urllib import request, parse
+
+    url = sys.argv[1]
+    email = {"email": sys.argv[2]}
+    header = parse.urlencode(email).encode()
+    with request.urlopen(request.Request(url, header)) as response: 
+        print(response.read().decode("utf-8"))
