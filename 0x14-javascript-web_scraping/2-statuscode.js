6d18f1d04777e05935cac617e7b9b020ba9c90c1
iman254 <stephaniemarquinz@icloud.com>
2023-04-27 02:25:31 +0300

    task 1

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// script that prints the error code from a Get request 
+
+const request = require('request');
+const url = process.argv[2];
+request(url, (err, res, body) => {
+  if (err) console.error(err);
+  console.log('code:', res.statusCode);
+});
