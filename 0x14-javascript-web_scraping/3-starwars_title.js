1a8e27580e5e20970796b0b23cc252095463fee8
iman254 <stephaniemarquinz@icloud.com>
2023-04-27 02:49:05 +0300

    task 4

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-// a script used in prints the title of a Star Wars movie where the episode number matches a given integer. 
+// a script used in prints the title of a Star Wars movie where the episode number matches a given integer.
 
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+// script that prints the number of movies where the character “Wedge Antilles” is present.
+
+const request = require('request');
+const url = process.argv[2];
+request(url, (err, res, body) => {
+  if (err) console.error(err);
+  const data = JSON.parse(body);
+  let count = 0;
+  for (let i = 0; i < data.results.length; i++) {
+    const chars = data.results[i].characters;
+    for (let k = 0; k < chars.length; k++) {
+      if (chars[k].includes('18')) {
+        count = count + 1;
+      }
+    }
+  }
+  console.log(count);
+});
+
