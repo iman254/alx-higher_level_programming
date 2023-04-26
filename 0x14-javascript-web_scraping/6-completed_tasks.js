a89b22c863d88d120a0e61de71326eaab20313d8
iman254 <stephaniemarquinz@icloud.com>
2023-04-27 02:56:02 +0300

    tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+// a script to gives the no of tas a user has performeD
+
+const request = require('request');
+const url = process.argv[2];
+request(url, (err, res, body) => {
+  if (err) console.error(err);
+  const tasks = JSON.parse(body);
+  const dict = {};
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].completed === true) {
+      if (!dict[tasks[i].userId]) dict[tasks[i].userId] = 0;
+      dict[tasks[i].userId] += 1;
+    }
+  }
+  console.log(dict);
+});
